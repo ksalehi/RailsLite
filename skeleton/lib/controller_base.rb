f1c775d56b012b1da9ebab31ff33a6a0ddae7aaf
Kia Salehi <kiasalehi@Kias-MacBook-Pro.local>
2016-06-08 15:32:41 -0700

    initial commit

---
+++ skeleton/lib/controller_base.rb
@@ -0,0 +1,64 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'active_support/inflector'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res, route_params = {})
+    @req = req
+    @res = res
+    @params = route_params.merge(req.params)
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response ||= false
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    raise Exception if already_built_response?
+    @res['Location'] = url
+    @res.status = 302
+    session.store_session(@res)
+    @already_built_response = true
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    raise Exception if already_built_response?
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    session.store_session(@res)
+    @already_built_response = true
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    controller = self.class.to_s # in inheriting object, will refer to UsersController, eg
+    controller_name = controller.underscore
+    path = "views/#{controller_name}/#{template_name}.html.erb"
+    template = File.read(path)
+    template_erb = ERB.new(template)
+    template_result = template_erb.result(binding)
+    render_content(template_result, 'text/html')
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
+  end
+end
